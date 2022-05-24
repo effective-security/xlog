812f17919a530125060e6ed3fbcee655a7050743
Denis Issoupov <denis@ekspand.com>
2022-05-25 00:05:28 +0100

    logs: stackdriver fields

--- stackdriver/sd_test.go
+++ stackdriver/sd_test.go
@@ -22,7 +22,7 @@ func Test_Formatter(t *testing.T) {
 	logger.Info("Test Info")
 	result := b.String()
 	assert.Contains(t, result, "{\"logName\":\"sd\",\"component\":\"stackdriver\",\"timestamp\":")
-	assert.Contains(t, result, "\"message\":\"Test Info\",\"severity\":\"INFO\",\"sourceLocation\":{\"function\":\"Test_Formatter\"}}\n")
+	assert.Contains(t, result, "\"textPayload\":\"src=Test_Formatter, Test Info\",\"severity\":\"INFO\",\"sourceLocation\":{\"function\":\"Test_Formatter\"}}\n")
 	b.Reset()
 
 	k3 := struct {
@@ -31,11 +31,11 @@ func Test_Formatter(t *testing.T) {
 
 	logger.KV(xlog.INFO, "k1", 1, "k2", false, "k3", k3)
 	result = b.String()
-	assert.Contains(t, result, "\"message\":\"k1=1, k2=false, k3={\\\"Foo\\\":\\\"bar\\\"}\",\"severity\":\"INFO\",\"sourceLocation\":{\"function\":\"Test_Formatter\"}}\n")
+	assert.Contains(t, result, "\"textPayload\":\"src=Test_Formatter, k1=1, k2=false, k3={\\\"Foo\\\":\\\"bar\\\"}\",\"severity\":\"INFO\",\"sourceLocation\":{\"function\":\"Test_Formatter\"}}\n")
 	b.Reset()
 
 	logger.KV(xlog.ERROR, "err", fmt.Errorf("log error"))
 	result = b.String()
-	assert.Contains(t, result, "\"message\":\"err=\\\"log error\\\"\",\"severity\":\"ERROR\",\"sourceLocation\":{\"file\":\"sd_test.go\",\"line\":37,\"function\":\"Test_Formatter\"}}\n")
+	assert.Contains(t, result, "\"textPayload\":\"src=Test_Formatter, err=\\\"log error\\\"\",\"severity\":\"ERROR\",\"sourceLocation\":{\"file\":\"sd_test.go\",\"line\":37,\"function\":\"Test_Formatter\"}}\n")
 	b.Reset()
 }
