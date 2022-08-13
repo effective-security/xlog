8de4392b329f0b6b2a600c04bb472aaf5bf352ec
Denis Issoupov <denis@averlon.io>
2022-08-13 11:22:25 +0100

    added json formatter

--- example_test.go
+++ example_test.go
@@ -34,6 +34,14 @@ func ExampleStringFormatter() {
 
 	logger.KV(xlog.ERROR, "reason", "with time, level, caller", "err", errors.New("just a string").Error(), "number", 123)
 
+	f.Options(xlog.FormatWithLocation)
+	logger.KV(xlog.ERROR, "reason", "location",
+		"err", errors.New("just a string").Error(),
+		"number", 123,
+		"list", list,
+		"obj", obj,
+	)
+
 	f.Options(xlog.FormatSkipTime, xlog.FormatSkipLevel, xlog.FormatNoCaller)
 	logger.KV(xlog.ERROR, "reason", "skip time, level, caller",
 		"err", errors.New("just a string").Error(),
@@ -43,10 +51,11 @@ func ExampleStringFormatter() {
 	)
 
 	// Output:
-	// time=2021-04-01T00:00:00Z level=W pkg=format src=ExampleStringFormatter "string1" "string 2" ["item 1","item 2"] {"Foo":"foo","Bar":5}
-	// time=2021-04-01T00:00:00Z level=W pkg=format_kv src=ExampleStringFormatter key1="value 2" key2=2 list=["item 1","item 2"] obj={"Foo":"foo","Bar":5}
-	// time=2021-04-01T00:00:00Z level=E pkg=string_formatter src=ExampleStringFormatter prefix="addon" reason="with time, level, caller" err="just a string" number=123
-	// pkg=string_formatter prefix="addon" reason="skip time, level, caller" err="just a string" number=123 list=["item 1","item 2"] obj={"Foo":"foo","Bar":5}
+	// time=2021-04-01T00:00:00Z level=W pkg=format func=ExampleStringFormatter "string1" "string 2" ["item 1","item 2"] {"Foo":"foo","Bar":5}
+	// time=2021-04-01T00:00:00Z level=W pkg=format_kv func=ExampleStringFormatter key1="value 2" key2=2 list=["item 1","item 2"] obj={"Foo":"foo","Bar":5}
+	// time=2021-04-01T00:00:00Z level=E pkg=string_formatter func=ExampleStringFormatter prefix="addon" reason="with time, level, caller" err="just a string" number=123
+	// time=2021-04-01T00:00:00Z level=E pkg=string_formatter src=example_test.go:38 func=ExampleStringFormatter prefix="addon" reason="location" err="just a string" number=123 list=["item 1","item 2"] obj={"Foo":"foo","Bar":5}
+	// pkg=string_formatter src=example_test.go:46 prefix="addon" reason="skip time, level, caller" err="just a string" number=123 list=["item 1","item 2"] obj={"Foo":"foo","Bar":5}
 }
 
 func ExamplePrettyFormatter() {
@@ -71,6 +80,14 @@ func ExamplePrettyFormatter() {
 
 	logger.KV(xlog.ERROR, "reason", "with time, level, caller", "err", errors.New("just a string").Error(), "number", 123)
 
+	f.Options(xlog.FormatWithLocation)
+	logger.KV(xlog.ERROR, "reason", "location",
+		"err", errors.New("just a string").Error(),
+		"number", 123,
+		"list", list,
+		"obj", obj,
+	)
+
 	f.Options(xlog.FormatSkipTime, xlog.FormatSkipLevel, xlog.FormatNoCaller)
 	logger.KV(xlog.ERROR, "reason", "skip time, level, caller",
 		"err", errors.New("just a string").Error(),
@@ -80,8 +97,55 @@ func ExamplePrettyFormatter() {
 	)
 
 	// Output:
-	// 2021-04-01 00:00:00.000000 W | format: src=ExamplePrettyFormatter, string1, string 2, [item 1 item 2], {foo 5 shout not print}
-	// 2021-04-01 00:00:00.000000 W | format_kv: src=ExamplePrettyFormatter, key1="value 2", key2=2, list=["item 1","item 2"], obj={"Foo":"foo","Bar":5}
-	// 2021-04-01 00:00:00.000000 E | pretty_formatter: src=ExamplePrettyFormatter, reason="with time, level, caller", err="just a string", number=123
-	// pretty_formatter: reason="skip time, level, caller", err="just a string", number=123, list=["item 1","item 2"], obj={"Foo":"foo","Bar":5}
+	// 2021-04-01 00:00:00.000000 W | format: func=ExamplePrettyFormatter, "string1", "string 2", ["item 1","item 2"], {"Foo":"foo","Bar":5}
+	// 2021-04-01 00:00:00.000000 W | format_kv: func=ExamplePrettyFormatter, key1="value 2", key2=2, list=["item 1","item 2"], obj={"Foo":"foo","Bar":5}
+	// 2021-04-01 00:00:00.000000 E | pretty_formatter: func=ExamplePrettyFormatter, reason="with time, level, caller", err="just a string", number=123
+	// 2021-04-01 00:00:00.000000 E | pretty_formatter: src=example_test.go:84, func=ExamplePrettyFormatter, reason="location", err="just a string", number=123, list=["item 1","item 2"], obj={"Foo":"foo","Bar":5}
+	// pretty_formatter: src=example_test.go:92, reason="skip time, level, caller", err="just a string", number=123, list=["item 1","item 2"], obj={"Foo":"foo","Bar":5}
+}
+
+func ExampleJSONFormatter() {
+	xlog.TimeNowFn = func() time.Time {
+		date, _ := time.Parse("2006-01-02", "2021-04-01")
+		return date
+	}
+
+	var logger = xlog.NewPackageLogger("github.com/effective-security/xlog", "json_formatter")
+	f := xlog.NewJSONFormatter(os.Stdout)
+	xlog.SetFormatter(f)
+
+	list := []string{"item 1", "item 2"}
+	obj := struct {
+		Foo     string
+		Bar     int
+		private string
+	}{"foo", 5, "shoud not print"}
+
+	f.Format("format", xlog.WARNING, 1, "string1", "string 2", list, obj)
+	f.FormatKV("format_kv", xlog.WARNING, 1, "key1", "value 2", "key2", 2, "list", list, "obj", obj)
+
+	logger.KV(xlog.ERROR, "reason", "with time, level, caller", "err", errors.New("just a string").Error(), "number", 123)
+
+	f.Options(xlog.FormatWithLocation)
+	logger.KV(xlog.ERROR, "reason", "location",
+		"err", errors.New("just a string").Error(),
+		"number", 123,
+		"list", list,
+		"obj", obj,
+	)
+
+	f.Options(xlog.FormatSkipTime, xlog.FormatSkipLevel, xlog.FormatNoCaller)
+	logger.KV(xlog.ERROR, "reason", "skip time, level, caller",
+		"err", errors.New("just a string").Error(),
+		"number", 123,
+		"list", list,
+		"obj", obj,
+	)
+
+	// Output:
+	// {"func":"ExampleJSONFormatter","level":"W","msg":"string1string 2[item 1 item 2] {foo 5 shoud not print}","pkg":"format","time":"2021-04-01T00:00:00Z"}
+	// {"func":"ExampleJSONFormatter","key1":"value 2","key2":2,"level":"W","list":["item 1","item 2"],"obj":{"Foo":"foo","Bar":5},"pkg":"format_kv","time":"2021-04-01T00:00:00Z"}
+	// {"err":"just a string","func":"ExampleJSONFormatter","level":"E","number":123,"pkg":"json_formatter","reason":"with time, level, caller","time":"2021-04-01T00:00:00Z"}
+	// {"err":"just a string","func":"ExampleJSONFormatter","level":"E","list":["item 1","item 2"],"number":123,"obj":{"Foo":"foo","Bar":5},"pkg":"json_formatter","reason":"location","src":"example_test.go:130","time":"2021-04-01T00:00:00Z"}
+	// {"err":"just a string","list":["item 1","item 2"],"number":123,"obj":{"Foo":"foo","Bar":5},"pkg":"json_formatter","reason":"skip time, level, caller","src":"example_test.go:138"}
 }
---
+++ json_formatter.go
@@ -0,0 +1,116 @@
+// Copyright 2015 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xlog
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+)
+
+// NewJSONFormatter returns an instance of JsonFormatter
+func NewJSONFormatter(w io.Writer) Formatter {
+	return &JSONFormatter{
+		w: bufio.NewWriter(w),
+		config: config{
+			withCaller:   true,
+			skipTime:     false,
+			withLocation: false,
+			color:        false,
+		},
+	}
+}
+
+// JSONFormatter provides default logs format
+type JSONFormatter struct {
+	config
+	w *bufio.Writer
+}
+
+// Options allows to configure formatter behavior
+func (c *JSONFormatter) Options(ops ...FormatterOption) Formatter {
+	c.config.options(ops)
+	return c
+}
+
+// FormatKV log entry string to the stream,
+// the entries are key/value pairs
+func (c *JSONFormatter) FormatKV(pkg string, l LogLevel, depth int, entries ...interface{}) {
+	m := kvToMap(entries...)
+	c.format(pkg, l, depth+1, false, m)
+}
+
+// Format log entry string to the stream
+func (c *JSONFormatter) Format(pkg string, l LogLevel, depth int, entries ...interface{}) {
+	c.format(pkg, l, depth+1, true, map[string]interface{}{}, entries...)
+}
+
+// Format log entry string to the stream
+func (c *JSONFormatter) format(pkg string, l LogLevel, depth int, escape bool, kv map[string]interface{}, entries ...interface{}) {
+	if !c.skipTime {
+		now := TimeNowFn().UTC()
+		kv["time"] = now.Format(time.RFC3339)
+	}
+	if !c.skipLevel {
+		kv["level"] = l.Char()
+	}
+	if pkg != "" {
+		kv["pkg"] = pkg
+	}
+
+	if c.withLocation {
+		file, line := location(depth + 1) // It's always the same number of frames to the user's call.
+		kv["src"] = fmt.Sprintf("%s:%d", file, line)
+	}
+
+	if c.withCaller {
+		kv["func"] = callerName(depth + 1)
+	}
+
+	if len(entries) > 0 {
+		kv["msg"] = fmt.Sprint(entries...)
+	}
+
+	encoder := json.NewEncoder(c.w)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(kv)
+
+	c.Flush()
+}
+
+// Flush the logs
+func (c *JSONFormatter) Flush() {
+	c.w.Flush()
+}
+
+func kvToMap(kvList ...interface{}) map[string]interface{} {
+	size := len(kvList)
+	m := make(map[string]interface{})
+
+	for i := 0; i < size; i += 2 {
+		k, ok := kvList[i].(string)
+		if !ok {
+			panic(fmt.Sprintf("key is not a string: %v", EscapedString(kvList[i])))
+		}
+		var v interface{}
+		if i+1 < size {
+			v = kvList[i+1]
+		}
+		m[k] = v
+	}
+	return m
+}
