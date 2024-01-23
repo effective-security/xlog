42acb17a0586ab7e0b6b85cba9b4e37ee9671dd1
Denis Issoupov <denis@ekspand.com>
2024-01-23 17:21:33 +0000

    Fix stackdriver logs

--- stackdriver/sd_test.go
+++ stackdriver/sd_test.go
@@ -24,7 +24,7 @@ func Test_FormatterOptions(t *testing.T) {
 
 	logger.KV(xlog.INFO, "k1", 1, "k2", false, "nil", nil, "empty", "")
 	result := b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","jsonPayload":{"k1":1,"k2":false,"nil":null,"empty":""},"severity":"INFO","sourceLocation":{"file":"sd_test.go","line":25,"function":"Test_FormatterOptions"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","message":{"k1":1,"k2":false,"nil":null,"empty":""},"severity":"INFO","sourceLocation":{"file":"sd_test.go","line":25,"function":"Test_FormatterOptions"}}`+"\n", result)
 	b.Reset()
 
 	xlog.SetFormatter(NewFormatter(writer, "sd").
@@ -32,7 +32,7 @@ func Test_FormatterOptions(t *testing.T) {
 
 	logger.KV(xlog.INFO, "k1", 1, "k2", false, "nil", nil, "empty", "")
 	result = b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","jsonPayload":{"k1":1,"k2":false,"nil":null,"empty":""},"severity":"INFO","sourceLocation":{"function":"Test_FormatterOptions"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","message":{"k1":1,"k2":false,"nil":null,"empty":""},"severity":"INFO","sourceLocation":{"function":"Test_FormatterOptions"}}`+"\n", result)
 	b.Reset()
 
 	xlog.SetFormatter(NewFormatter(writer, "sd").
@@ -50,7 +50,7 @@ func Test_FormatterOptions(t *testing.T) {
 
 	logger.KV(xlog.INFO, "k1", 1, "k2", false, "nil", nil, "empty", "", "zero", 0, "struct", stru)
 	result = b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","jsonPayload":{"k1":1,"k2":false,"zero":0,"struct":{"A":"a","B":1,"C":1.1,"D":123,"E":{},"Is":false,"Dur":1000000000}},"severity":"INFO","sourceLocation":{"function":"Test_FormatterOptions"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","message":{"k1":1,"k2":false,"zero":0,"struct":{"A":"a","B":1,"C":1.1,"D":123,"E":{},"Is":false,"Dur":1000000000}},"severity":"INFO","sourceLocation":{"function":"Test_FormatterOptions"}}`+"\n", result)
 	b.Reset()
 
 	assert.Panics(t, func() {
@@ -77,7 +77,7 @@ func Test_Formatter(t *testing.T) {
 
 	logger.Info("Test Info")
 	result := b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","jsonPayload":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"Test_Formatter"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","message":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"Test_Formatter"}}`+"\n", result)
 	b.Reset()
 
 	k3 := struct {
@@ -86,12 +86,12 @@ func Test_Formatter(t *testing.T) {
 
 	logger.KV(xlog.INFO, "k1", 1, "k2", false, "k3", k3, "nil", nil, "empty", "")
 	result = b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","jsonPayload":{"k1":1,"k2":false,"k3":{"Foo":"bar"}},"severity":"INFO","sourceLocation":{"function":"Test_Formatter"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","message":{"k1":1,"k2":false,"k3":{"Foo":"bar"}},"severity":"INFO","sourceLocation":{"function":"Test_Formatter"}}`+"\n", result)
 	b.Reset()
 
 	logger.KV(xlog.ERROR, "err", fmt.Errorf("log error"))
 	result = b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","jsonPayload":{"err":"log error"},"severity":"ERROR","sourceLocation":{"file":"sd_test.go","line":92,"function":"Test_Formatter"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","message":{"err":"log error"},"severity":"ERROR","sourceLocation":{"file":"sd_test.go","line":92,"function":"Test_Formatter"}}`+"\n", result)
 	b.Reset()
 }
 
@@ -113,7 +113,7 @@ func Test_FormatterFunc(t *testing.T) {
 		logger.Info("Test Info")
 	}()
 	result := b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","jsonPayload":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"Test_FormatterFunc.func3"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","message":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"Test_FormatterFunc.func3"}}`+"\n", result)
 	b.Reset()
 
 	func() {
@@ -121,7 +121,7 @@ func Test_FormatterFunc(t *testing.T) {
 		s.log("Test Info")
 	}()
 	result = b.String()
-	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","jsonPayload":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"log"}}`+"\n", result)
+	assert.Equal(t, `{"logName":"sd","component":"stackdriver","timestamp":"2019-01-01T00:00:00Z","message":{"msg":"Test Info"},"severity":"INFO","sourceLocation":{"function":"log"}}`+"\n", result)
 }
 
 type someSvc struct{}
